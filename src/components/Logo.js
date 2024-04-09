80b554081c84a03fa4a4277228668fd208840ffd
Tyler Olmsted <profbass@gmail.com>
2024-04-09 16:36:06 -0600

    Navigation and logo animations added

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,33 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 function Logo() {
     return (
-        <div>
-            <a href="/">
-                <img src={process.env.PUBLIC_URL + '/img/logo-tyler-olmsted.svg'} className="w-20 lg:w-24 hover: transform hover:scale-105 transition-all" alt="Tyler Olmsted Logo" />
-            </a>
-        </div>
+        <AnimatePresence>
+            <motion.div
+                initial={{ 
+                    y: -100,
+                    transition: {
+                        duration: 0.5
+                    }
+                }}
+                animate={{ 
+                    y: 0,
+                    transition: {
+                        duration: 1,
+                        bounce: 0.4,
+                        type: "spring"
+                    } 
+                }}
+                exit={{ 
+                    y: -100 
+                }}
+                whileHover={{ scale: 1.1 }}
+            >
+                <a href="/">
+                    <img src={process.env.PUBLIC_URL + '/img/logo-tyler-olmsted.svg'} className="w-20 lg:w-24" alt="Tyler Olmsted Logo" />
+                </a>
+            </motion.div>
+        </AnimatePresence>
     );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
-import { Link } from 'react-scroll'; 
-
+import { Link } from 'react-scroll';
+import { motion } from 'framer-motion';
 
 const menuItems = [
     { name: 'Home', href: 'home', id: 1 },
@@ -9,22 +9,57 @@ const menuItems = [
 ];
 
 function Navigation() {
+    // Animation variants for the container
+    const containerVariants = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.1, // Stagger the animation of children
+                delayChildren: 0.75, // Delay the animation of children
+            },
+        },
+    };
+
+    // Animation variants for each item
+    const itemVariants = {
+        hidden: { y: -50, opacity: 0 }, // Start above the original position
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: 'spring',
+                stiffness: 100,
+            },
+        },
+        hover: {
+            scale: 1.1,
+            transition: {
+                duration: 0.3,
+            },
+        }
+    };
 
     return (
-        <ul className="hidden sm:flex items-center space-x-8 navigation">
+        <motion.ul
+            className="hidden sm:flex items-center space-x-8 navigation"
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+        >
             {menuItems.map((item) => (
-                <li key={item.id}>
+                <motion.li key={item.id} variants={itemVariants}>
                     <Link
                         to={item.href}
                         smooth={true}
                         duration={500}
-                        className="font-body text-lg uppercase text-white hover:text-yellow"
+                        className="font-body text-lg uppercase text-white cursor-pointer hover:text-yellow transition-all duration-300"
                     >
                         {item.name}
                     </Link>
-                </li>
+                </motion.li>
             ))}
-        </ul>
+        </motion.ul>
     );
 }
 
